8f91670b40d14425d0586f848b8e8ac61dd0c5d9
Mark van Gelder <281043415+JanHertog@users.noreply.github.com>
2026-09-08 20:12:59 +0200

    Change audio source file name to soundfile.opus

--- source/app/1/page.tsx
+++ source/app/1/page.tsx
@@ -5,7 +5,7 @@ export default function Index() {
       <h1>Het audio bestand:</h1>
       
       <audio autoPlay controls>
-        <source src="PTT-20260830-WA0000.opus" type="audio/ogg; codecs=opus" />
+        <source src="soundfile.opus" type="audio/ogg; codecs=opus" />
         Your browser does not support the audio element.
       </audio>
     </>
